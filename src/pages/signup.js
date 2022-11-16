0c42390ec11d182f0dd7f9cf9101c73812a77793
Lgatehouse <98628563+Lgatehouse@users.noreply.github.com>
2022-11-16 08:18:46 -0700

    Registration page

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import './signup.css'
   
 const SignUp = () => {
   return (
     <div>
       <h1>Sign Up</h1>
+      <form id={"form"} onSubmit={submit}>
+          <input type={"text"} placeholder={"Full Name"} className={"input"}/>
+          <input type={"text"} placeholder={"Email"} className={"input"}/>
+          <input type={"text"} placeholder={"Phone Number"} className={"input"}/>
+          <input type={"text"} placeholder={"Password"} className={"input"}/>
+          <input type={"text"} placeholder={"Confirm Password"} className={"input"}/>
+          <input type={"submit"} className={"submit"}/>
+      </form>
     </div>
   );
 };
+
+function submit() {
+    let form = document.getElementById("form")
+    let inputs = Array.from(form.getElementsByTagName('input'));
+    for (let input of inputs){
+        if(input.getAttribute('type') !== 'submit'){
+            if(input.value === ""){
+                alert("Enter all fields before submitting")
+                break
+            }
+            console.log(input.value)
+        }
+
+    }
+}
+
+
   
 export default SignUp;
