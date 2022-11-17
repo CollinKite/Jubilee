0747660a1dc5470dca3ef81b3336e13e12ebd523
Collin Kite <me@collinkite.com>
2022-11-17 09:03:36 -0700

    Added Basic Toast to login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import Logo from '../components/logo';
 import './login.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+const notify = () => toast("Wow so easy!");
 
+toast.error('🦄 Wow so easy!', {
+  position: "bottom-center",
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: "dark",
+  });
   
 const login = () => {
   return (
@@ -14,12 +27,24 @@ const login = () => {
           <br/>
           <input id="password" type="password" placeholder="Password" />
           <br/>
-          <button id="submit">Login</button>
+          <button id="submit" onClick={notify}>Login</button>
           <br/>
           <a href="/forgot">Forgot Password?</a>
           <br/>
           <a href="/signup">Don't have an account?</a>
         </div>
+        <ToastContainer
+          position="bottom-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light  "
+        />
     </div>
   );
 };
