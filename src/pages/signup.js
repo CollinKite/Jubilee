0e1a123753d17c6cad9f1fc5dd9d7d4d8ba78275
CollinKite <me@collinkite.com>
2022-11-18 15:28:22 -0700

    Finished Sign Up Page and updated color for links

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -2,7 +2,53 @@ import React from 'react';
 import Logo from '../components/logo';
 import back from '../icons/arrow_back_48dp.svg'
 import './login.css'
-  
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const emptyError = () => toast.error('Please fill out all fields', {
+  position: "bottom-center",
+  autoClose: 2000,
+  hideProgressBar: true,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: "light",
+  });;
+
+  const passError = () => toast.error('Passwords Don\'t Match', {
+    position: "bottom-center",
+    autoClose: 2000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    });;
+
+const signUpSuccess = () => toast.success('Sign Up Success', {
+  position: "bottom-center",
+  autoClose: 5000,
+  hideProgressBar: true,
+  closeOnClick: true,
+  pauseOnHover: false,
+  draggable: true,
+  progress: undefined,
+  theme: "light",
+  });
+
+  const signUpError = () => toast.error('Error Communicating with server, please try again later', {
+    position: "bottom-center",
+    autoClose: 5000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    });;
+
 const SignUp = () => {
   return (
     <div>
@@ -10,39 +56,70 @@ const SignUp = () => {
       <div id="card">
         <a href='/' id='back'><img src={back} alt="back icon"></img></a>
         <h1>Sign Up</h1>
-        <form id={"form"} onSubmit={submit}>
-            <input type={"text"} placeholder={"Full Name"} className={"input"}/>
+            <input id="name" type={"text"} placeholder={"Full Name"} className={"input"}/>
             <br/>
-            <input type={"text"} placeholder={"Email"} className={"input"}/>
+            <input id="email" type={"text"} placeholder={"Email"} className={"input"}/>
             <br/>
-            <input type={"text"} placeholder={"Phone Number"} className={"input"}/>
+            <input id="phone" type={"text"} placeholder={"Phone Number"} className={"input"}/>
             <br/>
-            <input type={"password"} placeholder={"Password"} className={"input"}/>
+            <input id="pass" type={"password"} placeholder={"Password"} className={"input"}/>
             <br/>
-            <input type={"password"} placeholder={"Confirm Password"} className={"input"}/>
+            <input id="passConf" type={"password"} placeholder={"Confirm Password"} className={"input"}/>
             <br/> 
-            <button id="submit" onClick={"submit"}>Login</button>
-        </form>
+            <button id="submit" onClick={signup}>Sign Up</button>
       </div>
+      <ToastContainer limit={1}/>
     </div>
   );
 };
 
-function submit() {
-    let form = document.getElementById("form")
-    let inputs = Array.from(form.getElementsByTagName('input'));
-    for (let input of inputs){
-        if(input.getAttribute('type') !== 'submit'){
-            if(input.value === ""){
-                alert("Enter all fields before submitting")
-                break
-            }
-            console.log(input.value)
-        }
 
+async function signup() {
+  //create array of all input fields
+  document.getElementById("submit").disabled = true;
+  let inputs = document.getElementsByClassName("input");
+  for (let input of inputs){
+    if(input.value === ""){
+        emptyError();
+        document.getElementById("submit").disabled = false;
+        return;
     }
+  } 
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+  let pass = document.getElementById("pass").value;
+  let passConf = document.getElementById("passConf").value;
+
+  if (pass !== passConf){
+    passError();
+    document.getElementById("submit").disabled = false;
+    return;
+  }
+
+  let xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("POST", "http://localhost:8080/users/register");
+  xmlHttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xmlHttp.onreadystatechange = function() {
+    if (this.status === 200) {
+        console.log("sign up success");
+        signUpSuccess();
+        sleep(2000).then(() => {
+          window.location.replace("/");
+        });
+    }
+    else
+    {
+      console.log("error");
+      signUpError();
+      document.getElementById("submit").disabled = false;
+    }
+  }
+  xmlHttp.send(JSON.stringify({name, email, phone, pass}));    
 }
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-  
 export default SignUp;
