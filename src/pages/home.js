3703072b5548e72882b530839b1fc966f8c9dea5
Lgatehouse <98628563+Lgatehouse@users.noreply.github.com>
2022-12-01 07:30:53 -0700

    Started to add functionality

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,12 +7,40 @@ import { getActiveElement } from '@testing-library/user-event/dist/utils';
 import arrow from '../images/arrow.png'
 import remove from '../images/delete.png'
 
+const Home = () => {
+   
+    return (
+        <>
+                <div className='container'>
+                    <input type={"image"} src={require('../images/menu-icon.png')} className="menu" onClick={() => menuVisiblity("sidebar")}/>
+
+                    <div className='sidebar' id='sidebar'>
+                        <ul className='options'>
+                            <br/>
+                            <li><a href="/home" onClick={logout}>Logout</a></li>
+                        </ul>
+                        <ul className='groups'>
+                            <br/>
+                            <li><div className='groupLink'>Home</div></li>
+                        </ul>
+                        <input type={"text"} placeholder="Enter New Group" className='groupEdit'/>
+                    </div> 
+                    <div className='lists'>
+                        <ul id = "listHolder" onLoad={GetTasks}>
+                            <li></li>
+                        </ul>
+                    </div>
+                    <div className='infobar' id="infobar"> </div> 
+                </div>
+        </>
+    );
+
+}
+
 //this code can be replaced with better token authorization
 function loadToken()
 {
     var token = localStorage.getItem("token");
-
-    console.log(token)
 
     if(token !== null){
     var base64Url = token.split('.')[1];
@@ -36,159 +64,86 @@ function logout()
     window.location.replace = "/login";
 }
 
+function menuVisiblity(id) {
 
-const Home = () => {
-    const [loading, setLoading] = useState(true)
-
-    //just an example I was using for testing
-    let task = {
-        taskId: 1,
-        taskName: "Task 1",
-        taskDescription: "Stuff",
-        taskType: "Personal",
-        createDate: "2020-10-10",
-        dueDate: "2020-10-10",
-        completed: false
-    }
-
-    function menuVisiblity(id) {
-
-        var menu = document.getElementById(id)
-        menu.classList.toggle("hidden")
-    }
-
-    function infoBar() {
-
-        var info = document.getElementById("infobar")
-        info.classList.remove("hidden")
-        info.innerHTML = `<div class="header"> \
-        <input type="image" src="${arrow}" class="infoMenu" id="exit"/> \
-        <div class="createDate">Created: ${task.createDate}</div> \
-        <input type="image" src="${remove}" class="infoDelete" id="trash"/> \
-        </div> \
-            <ul> \
-            <li><input type="text" class="name" id="name"/></li> \
-            <li><input type="text" class="name" placeholder="Enter Due Date"/></li> \
-            <li><textarea class="description" placeholder="Enter task Description"></textarea></li> \
-            </ul>`
-
-        var exit = document.getElementById("exit")
-        exit.addEventListener("click", function() {menuVisiblity("infobar")}, false)
-        var trash = document.getElementById("trash")
-        trash.addEventListener("click", function() {menuVisiblity("infobar")}, false)
-        var name = document.getElementById("name")
-        name.setAttribute("value", task.taskName)
-        
-    }
-
-    
-
-    // useEffect(() => {
-    //   setTimeout(() => setLoading(false), 10000)
-    // }, [])
-
-    
-    document.onreadystatechange = function () {
-        console.log(this.readyState)
-          if (document.readyState === "complete") {
-            console.log("page fully loaded")
-            setLoading(false)
-          }
-        }
-    return (
-        <>
-            {loading === false ? (
-
-                <div className='container'>
-                    <input type={"image"} src={require('../images/menu-icon.png')} className="menu" onClick={() => menuVisiblity("sidebar")}/>
-
-                    <div className='sidebar' id='sidebar'>
-                        <ul className='options'>
-                            <br/>
-                            <li><a href="/home" onClick={logout}>Logout</a></li>
-                        </ul>
-                        <ul className='groups'>
-                            <br/>
-                            <li><div className='groupLink'>Home</div></li>
-                        </ul>
-                        <input type={"text"} placeholder="Enter New Group" className='groupEdit'/>
-
-                    </div> 
-                    <div className='lists'>
-                        <ul id = "listHolder" >
-                            <li>
-                                <div className='submitBox'>
-                                    <input type={"text"} placeholder="Enter New Task" className='newTask'/>
-                                    <button className='submit'>Add</button>
-                                </div>
-                                <br/>
-                            </li>
-                            {/* <li className='li' >
-                                <div className='info' id={task.taskId + "I"} onClick={() => infoBar("sidebar")}>
-                                    <div className='taskName'>
-                                        {task.taskName}
-                                    </div>
-                                </div>
-                            </li>
-                            <br/>
-                            <li className='li'>
-                                <div className='info' id={task.taskId + "I"}>
-                                    <div className='taskName'>
-                                        {task.taskName}
-                                    </div>
-                                </div>
-                            </li> */}
-                        </ul>
-                    </div>
-                    
-                    <div className='infobar' id="infobar">
-                        <div className='header'>
-                        <input type={"image"} src={require('../images/arrow.png')} className="infoMenu" onClick={() => menuVisiblity('infobar')}/>
-                        <div className="createDate">Created: {task.createDate}</div>
-                        <input type={"image"} src={require('../images/delete.png')} className="infoDelete" onClick={() => menuVisiblity('infobar')}/>
-                        </div>
-                            <ul>
-                            <li><input type={"text"} className="name" defaultValue={task.taskName}/></li>
-                            <li><input type={"text"} className="name" placeholder='Enter Due Date'/></li>
-                            <li><textarea className='description' placeholder='Enter task Description'/></li>
-                            <li>{task.completed}</li>
-                            </ul>
-                    </div> 
-                </div>
-
-                ) : (
-                    <BarLoader color="#36d7b7" />
-                )
-            }
-        </>
-    );
-
+    var menu = document.getElementById(id)
+    menu.classList.toggle("hidden")
 }
 
+function infoBar(item) {
 
-// GetTasks();
-// async function GetTasks() {
+    let task = item
+
+    var info = document.getElementById("infobar")
+    info.classList.remove("hidden")
+    info.innerHTML = `<div class="header"> \
+    <input type="image" src="${arrow}" class="infoMenu" id="exit"/> \
+    <div class="createDate">Created: ${task.createDate}</div> \
+    <input type="image" src="${remove}" class="infoDelete" id="trash"/> \
+    </div> \
+        <ul> \
+        <li><input type="text" class="name" id="name"/></li> \
+        <li><input type="text" class="name" id="date" placeholder="Enter Due Date"/></li> \
+        <li><textarea class="description" placeholder="Enter task Description" value = "what">${task.taskDescription}</textarea></li> \
+        </ul>`
+
+    document.getElementById("exit").addEventListener("click", function() {menuVisiblity("infobar")}, false)
+    document.getElementById("trash").addEventListener("click", function() {menuVisiblity("infobar")}, false)
+    document.getElementById("name").setAttribute("value", task.taskName)
+    document.getElementById("date").setAttribute("value", task.dueDate)
+    
+}
+
+function saveTask() {
+    console.log('stuff')
+}
+
+GetTasks()
+async function GetTasks() {
+
+    var tasks
+    await findUser().then(response => tasks = response.result.tasks)
   
-// let container = document.getElementById("listHolder")
-// container.innerHTML=`<li className='li' >
-// <div className='info' id={task.taskId + "I"} onClick={() => infoBar("sidebar")}>
-//     <div className='taskName'>
-//         {task.taskName}
-//     </div>
-// </div>
-// </li>` 
+    let container = document.getElementById("listHolder")
 
-// }
+    container.innerHTML = `<li>
+    <div className='submitBox'>
+        <input type="text" placeholder="Enter New Task" class='newTask'/>
+        <button class='submit' id="sbmtbtn">Add</button>
+    </div>
+    <br/>
+    </li>`
+    
+    tasks.forEach(element => {
+        container.innerHTML +=`<li class='li' >
+    <div class='info' id='${element.taskId}Info' >
+        <div class='taskName'>
+            ${element.taskName}
+        </div>
+    </div>
+    </li>
+    <br/>` 
 
-export default Home;
+    });
 
-findUser()
+    document.getElementById("sbmtbtn").addEventListener("click", function(){saveTask()}, false)
+    let elements = document.querySelectorAll('.info');
+
+   for(let i = 0; i < elements.length; i++){
+    elements[i].addEventListener('click', function() {infoBar(tasks[i])})
+   }
+}
 
 async function findUser() {
-    //Was having and issue connecting to api earlier so I was testing things with this
     let info = loadToken();
-    const res = await fetch(`http://localhost:8080/users/${info[0]}`)
-    const data = await res.json()
-    console.log(data)
-    return fetch(`http://localhost:8000/users/${info[0]}`)
+
+    return fetch(`http://localhost:8080/users/${info[0]}`)
+    .then((response) => response.json())
+    .then((data) => {
+        
+        return data
+    })
+    .catch(error => console.log(error))
 }
+
+export default Home;
